d24ba734013ec576223b3c4f352870b0b2a06c94
laxiy <forever.1025@outlook.jp>
2023-11-06 01:35:55 +0900

    add elements

--- diductor.js
+++ diductor.js
@@ -72,7 +72,28 @@ class MinHeap {
 const minHeap = new MinHeap();
 minHeap.insert([5, 'A']);
 minHeap.insert([2, 'B']);
-minHeap.insert([8, 'C']);
+minHeap.insert([8, 'E']);
+minHeap.insert([12, 'F']);
+minHeap.insert([24, 'G']);
+minHeap.insert([0, 'H']);
+minHeap.insert([3, 'I']);
+minHeap.insert([1, 'J']);
+minHeap.insert([6, 'K']);
+minHeap.insert([7, 'L']);
+minHeap.insert([19, 'M']);
+minHeap.insert([14, 'N']);
+minHeap.insert([10, 'O']);
+minHeap.insert([4, 'P']);
+minHeap.insert([15, 'Q']);
+minHeap.insert([26, 'R']);
+minHeap.insert([34, 'S']);
+minHeap.insert([9, 'T']);
+minHeap.insert([41, 'U']);
+minHeap.insert([11, 'V']);
+minHeap.insert([16, 'W']);
+minHeap.insert([91, 'X']);
+minHeap.insert([102, 'Y']);
+minHeap.insert([89, 'Z']);
 
 while (!minHeap.isEmpty()) {
     const [priority, element] = minHeap.pop();
